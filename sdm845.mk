cc73624636aa294782be16abff4cc9e0fd374482
Jake Weinstein <jake@aospa.co>
2019-10-16 06:54:20 +0200

    sdm845-common: Do not balance msm_drm and kgsl-3d0 IRQs
    These are critical for display performance and should not be balanced
    in order to improve latency and responsiveness.
    
    Change-Id: I811699d0181427de21830c4ccbaf520ecea9e91a

--- sdm845.mk
+++ sdm845.mk
@@ -349,3 +349,5 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     RemovePackages
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
